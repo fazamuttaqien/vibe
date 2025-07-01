17794c203deb4c5040222a0728195f6748e665a9
fazamuttaqien <fazamttqn@gmail.com>
2025-07-01 15:23:17 +0700

    07: messages

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,9 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-import { helloworld } from "../../../inngest/functions";
+import { codeAgentFunction } from "@/inngest/functions";
 
 // Create an API that serves zero function
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloworld],
+  functions: [codeAgentFunction],
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
-export default function Home() {
+const Page = () => {
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const invoke = useMutation(
-    trpc.invoke.mutationOptions({
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
       onSuccess: () => {
-        toast.success("Background job started");
+        toast.success("Message created");
       },
     })
   );
@@ -23,11 +23,11 @@ export default function Home() {
     <div className="p-4 max-w-7xl mx-auto">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
-        disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ value: value })}
+        disabled={createMessage.isPending}
+        onClick={() => createMessage.mutate({ value: value })}
       >
-        Invoke Background Job
+        Invoke background jobs
       </Button>
     </div>
   );
-}
+};
